1dfd398760c88c5abf05a0bc2cdaf30ec69a142b
maram <maramalm367@gmail.com>
2024-12-23 14:33:51 +0300

    ...

--- lib/core/routes/app_routes.dart
+++ lib/core/routes/app_routes.dart
@@ -1,14 +1,5 @@
-import 'package:delivery_app/features/auth/presentation/view/forget_password.dart';
-import 'package:delivery_app/features/auth/presentation/view/sign_in_view.dart';
-import 'package:delivery_app/features/auth/presentation/view/sign_up_view.dart';
-import 'package:delivery_app/features/favorite/favorite_view.dart';
-import 'package:delivery_app/features/home/presentation/views/home_view.dart';
-import 'package:delivery_app/features/markets/presentation/views/markets_view.dart';
-import 'package:delivery_app/features/on_boarding/presentation/views/on_boarding_view.dart';
-import 'package:delivery_app/features/profile/presentation/view/profile_view.dart';
-import 'package:delivery_app/features/splash/presentation/view/splash_view.dart';
-import 'package:delivery_app/features/user/user_view.dart';
-import 'package:delivery_app/order/order_status.dart';
+import 'package:delivery_app_new/features/on_boarding/presentation/views/on_boarding_view.dart';
+import 'package:delivery_app_new/features/splash/presentation/view/splash_view.dart';
 import 'package:go_router/go_router.dart';
 
 final GoRouter router = GoRouter(
@@ -22,40 +13,40 @@ final GoRouter router = GoRouter(
         path: "/onBoarding",
         builder: (context, state) => const OnBoardingView(),
       ),
-      GoRoute(
-        path: "/SignUpView",
-        builder: (context, state) => const SignUpView(),
-      ),
-      GoRoute(
-        path: "/SignInView",
-        builder: (context, state) => const SignInView(),
-      ),
-      GoRoute(
-        path: "/HomeView",
-        builder: (context, state) => const HomeView(),
-      ),
-      GoRoute(
-        path: "/FavoriteView",
-        builder: (context, state) => const FavoriteView(),
-      ),
-      GoRoute(
-        path: "/UserView",
-        builder: (context, state) => const UserView(),
-      ),
-      GoRoute(
-        path: "/MarketsView",
-        builder: (context, state) => const MarketsView(),
-      ),
-      GoRoute(
-        path: "/ForgetPasswordView",
-        builder: (context, state) => const ForgetPasswordView(),
-      ),
-      GoRoute(
-        path: "/profileView",
-        builder: (context, state) => const ProfileView(),
-      ),
-      GoRoute(
-        path: "/OrderStatus",
-        builder: (context, state) => const OrderStatus(status: 'canceled'),
-      ),
+      // GoRoute(
+      //   path: "/SignUpView",
+      //   builder: (context, state) => const SignUpView(),
+      // ),
+      // GoRoute(
+      //   path: "/SignInView",
+      //   builder: (context, state) => const SignInView(),
+      // ),
+      // GoRoute(
+      //   path: "/HomeView",
+      //   builder: (context, state) => const HomeView(),
+      // ),
+      // GoRoute(
+      //   path: "/FavoriteView",
+      //   builder: (context, state) => const FavoriteView(),
+      // ),
+      // GoRoute(
+      //   path: "/UserView",
+      //   builder: (context, state) => const UserView(),
+      // ),
+      // GoRoute(
+      //   path: "/MarketsView",
+      //   builder: (context, state) => const MarketsView(),
+      // ),
+      // GoRoute(
+      //   path: "/ForgetPasswordView",
+      //   builder: (context, state) => const ForgetPasswordView(),
+      // ),
+      // GoRoute(
+      //   path: "/profileView",
+      //   builder: (context, state) => const ProfileView(),
+      // ),
+      // GoRoute(
+      //   path: "/OrderStatus",
+      //   builder: (context, state) => const OrderStatus(status: 'canceled'),
+      // ),
     ]);
